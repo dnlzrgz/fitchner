af1a9094bc3af081edd616e9de6a6a4340a38507
danielkvist <d94.zaragoza@gmail.com>
2019-01-24 19:28:11 +0100

    add documentation

--- fitchner.go
+++ fitchner.go
@@ -1,3 +1,4 @@
+// Package fitchner provides utilities for work with HTTP requests.
 package fitchner
 
 import (
@@ -5,13 +6,17 @@ import (
 	"net/http"
 )
 
+// Fetch receives an *http.Request and returns an *http.Response
+// or an error if the request gets a bad Status Code or the
+// *http.Request received is not valid.
 func Fetch(req *http.Request) (*http.Response, error) {
 	client := &http.Client{}
 	resp, err := client.Do(req)
-	defer resp.Body.Close()
 	if err != nil {
 		return nil, fmt.Errorf("bad request: %v", err)
 	}
+
+	defer resp.Body.Close()
 
 	if checkBadStatus(resp.StatusCode) {
 		return nil, fmt.Errorf("bad status code %v at %v", resp.StatusCode, req.URL)
