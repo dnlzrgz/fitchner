63b26e00925c8f9e89522a61f98a30c9a8dc1f50
danielkvist <d94.zaragoza@gmail.com>
2019-01-26 13:34:46 +0100

    add basic HTML parser

--- fitchner.go
+++ fitchner.go
@@ -4,6 +4,8 @@ package fitchner
 import (
 	"fmt"
 	"net/http"
+
+	"golang.org/x/net/html"
 )
 
 // Fetch receives an *http.Request and returns an *http.Response
@@ -13,16 +15,39 @@ func Fetch(req *http.Request) (*http.Response, error) {
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
+		resp.Body.Close()
 		return nil, fmt.Errorf("bad request: %v", err)
 	}
 
-	defer resp.Body.Close()
-
 	if checkBadStatus(resp.StatusCode) {
+		resp.Body.Close()
 		return nil, fmt.Errorf("bad status code %v at %v", resp.StatusCode, req.URL)
 	}
 
+	// It's responsability of the caller to close the body of the resp.
 	return resp, nil
+}
+
+// HTMLParser parses an *http.Response and returns an slice of html.Token
+// with all the StartTagToken (<a> by example) found
+// and closes the body of the *http.Response.
+func HTMLParser(r *http.Response) []html.Token {
+	b := html.NewTokenizer(r.Body)
+	defer r.Body.Close()
+
+	parsed := []html.Token{}
+
+	for {
+		tt := b.Next()
+
+		switch {
+		case tt == html.ErrorToken:
+			return parsed
+		case tt == html.StartTagToken:
+			t := b.Token()
+			parsed = append(parsed, t)
+		}
+	}
 }
 
 func checkBadStatus(s int) bool {
