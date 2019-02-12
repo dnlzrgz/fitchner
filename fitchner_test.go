2409512cbdf81f3ec63473d23b0fe1f9bf543c70
danielkvist <d94.zaragoza@gmail.com>
2019-02-12 12:52:39 +0100

    add tests to Filter
    Add test to Filter with no params and with only the attr param.

--- fitchner_test.go
+++ fitchner_test.go
@@ -35,8 +35,12 @@ func TestFetch(t *testing.T) {
 	}
 }
 
-func TestNodes(t *testing.T) {
-	nodes := testNodes(t)
+func TestFilterEmpty(t *testing.T) {
+	b := testFetch(t)
+	nodes, err := Filter(b, "", "")
+	if err != nil {
+		t.Errorf("while filtering: %v", err)
+	}
 
 	tests := []struct {
 		data string
@@ -47,6 +51,85 @@ func TestNodes(t *testing.T) {
 		{data: "title"},
 		{data: "body"},
 		{data: "header"},
+		{
+			data: "h1",
+			attr: []html.Attribute{
+				html.Attribute{
+					Key: "class",
+					Val: "title",
+				},
+			},
+		},
+		{
+			data: "a",
+			attr: []html.Attribute{
+				html.Attribute{
+					Key: "href",
+					Val: "https://www.google.com",
+				},
+				html.Attribute{
+					Key: "class",
+					Val: "link",
+				},
+				html.Attribute{
+					Key: "id",
+					Val: "link",
+				},
+			},
+		},
+		{
+			data: "a",
+			attr: []html.Attribute{
+				html.Attribute{
+					Key: "href",
+					Val: "mailto:testing@test.com",
+				},
+				html.Attribute{
+					Key: "class",
+					Val: "link mail",
+				},
+				html.Attribute{
+					Key: "id",
+					Val: "mail",
+				},
+			},
+		},
+	}
+
+	for i, tt := range tests {
+		n := nodes[i]
+
+		if tt.data != n.Data {
+			t.Errorf("expected node %q. got %q", tt.data, n.Data)
+		}
+
+		if len(tt.attr) != len(n.Attr) {
+			t.Errorf("expected node %q to have %v attributes. got %v", n.Data, len(tt.attr), len(n.Attr))
+		}
+
+		for j, attr := range tt.attr {
+			if attr.Key != n.Attr[j].Key {
+				t.Errorf("expected node %q to have attribute %q. got %q", n.Data, attr.Key, n.Attr[j].Key)
+			}
+
+			if attr.Val != n.Attr[j].Val {
+				t.Errorf("expected node %q to have attribute %q with value %q. got %q", n.Data, attr.Key, attr.Val, n.Attr[j].Val)
+			}
+		}
+	}
+}
+
+func TestFilterAttr(t *testing.T) {
+	b := testFetch(t)
+	nodes, err := Filter(b, "class", "")
+	if err != nil {
+		t.Errorf("while filtering: %v", err)
+	}
+
+	tests := []struct {
+		data string
+		attr []html.Attribute
+	}{
 		{
 			data: "h1",
 			attr: []html.Attribute{
@@ -156,17 +239,6 @@ func testFetch(t *testing.T) []byte {
 	return b
 }
 
-func testNodes(t *testing.T) []*html.Node {
-	b := testFetch(t)
-
-	nodes, err := Nodes(b)
-	if err != nil {
-		t.Errorf("while extracting nodes: %v", err)
-	}
-
-	return nodes
-}
-
 func BenchmarkFetch(b *testing.B) {
 	handler := testHandler()
 	server := httptest.NewServer(http.HandlerFunc(handler))
@@ -180,26 +252,5 @@ func BenchmarkFetch(b *testing.B) {
 	client := &http.Client{}
 	for i := 0; i < b.N; i++ {
 		Fetch(client, req)
-	}
-}
-
-func BenchmarkNodes(b *testing.B) {
-	handler := testHandler()
-	server := httptest.NewServer(http.HandlerFunc(handler))
-	defer server.Close()
-
-	req, err := http.NewRequest("GET", server.URL, nil)
-	if err != nil {
-		b.Fatalf("while creating a new request: %v", err)
-	}
-
-	client := &http.Client{}
-	body, err := Fetch(client, req)
-	if err != nil {
-		b.Fatalf("while making a new fetch: %v", err)
-	}
-
-	for i := 0; i < b.N; i++ {
-		Nodes(body)
 	}
 }
